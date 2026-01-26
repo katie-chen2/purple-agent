1e265bceb45c3b051f48ea03a951085d6ad0df48
katie-chen2 <cweiyuan9@gmail.com>
2026-01-25 19:13:54 -0500

    purple agent card

--- src/server.py
+++ src/server.py
@@ -16,7 +16,7 @@ from executor import Executor
 def main():
     parser = argparse.ArgumentParser(description="Run the A2A agent.")
     parser.add_argument("--host", type=str, default="127.0.0.1", help="Host to bind the server")
-    parser.add_argument("--port", type=int, default=9009, help="Port to bind the server")
+    parser.add_argument("--port", type=int, default=8008, help="Port to bind the server")
     parser.add_argument("--card-url", type=str, help="URL to advertise in the agent card")
     args = parser.parse_args()
 
@@ -24,16 +24,16 @@ def main():
     # See: https://a2a-protocol.org/latest/tutorials/python/3-agent-skills-and-card/
     
     skill = AgentSkill(
-        id="",
-        name="",
-        description="",
+        id="maximize-memory-occupation",
+        name="Memory Maximizer",
+        description="This purple agent designs instructions to take up as much as possible of the shared context memory for all purple agents.",
         tags=[],
         examples=[]
     )
 
     agent_card = AgentCard(
-        name="",
-        description="",
+        name="Purple Memory Maximizer",
+        description="This purple agent designs instructions to take up as much as possible of the shared context memory for all purple agents.",
         url=args.card_url or f"http://{args.host}:{args.port}/",
         version='1.0.0',
         default_input_modes=['text'],
